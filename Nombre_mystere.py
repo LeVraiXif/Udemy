c5a1886c348a488366a734e5789e8e48261230d4
Xif <tmxif732@gmail.com>
2022-10-17 16:30:30 +0200

    Jeu du nombre mystère (Update #1)

--- Nombre_mystere.py
+++ Nombre_mystere.py
@@ -7,19 +7,21 @@ print("Bienvenue sur le jeu du nombre mystère !")
 while try_to_find > 0:
     print(f"Il te reste {try_to_find} essais")
     reply = input("Le nombre est : ")
-    if not reply.isdigit():
-        while reply.isdigit() == False:
-            print("Ce n'est pas un chiffre")
-            reply = input("Le nombre est : ")
+    while not reply.isdigit():
+        print("Ce n'est pas un chiffre")
+        reply = input("Le nombre est : ")
     if not reply == number:
         try_to_find = try_to_find - 1
         if try_to_find == 0:
-            print(f"Le nombre étais {number}")
+            print(f"Le nombre était {number}")
         if int(reply) > number:
             print("Le nombre mystère est plus petit")
         elif int(reply) < number:
             print("Le nombre mystère est plus grand")
         elif int(reply) == number:
-            print(f"Bravo ! Tu à gagné il te reste encore {try_to_find} essaie")
+            if try_to_find > 1:
+                print(f"Bravo ! Tu as gagné il te reste encore {try_to_find} essais")
+            else:
+                print(f"Bravo ! Tu as gagné il te reste encore {try_to_find} essai")
             break
 
